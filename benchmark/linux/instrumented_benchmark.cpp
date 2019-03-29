3f7504eb6aafa64373d172ecf3aaf26753c42960
Daniel Lemire <lemire@gmail.com>
2019-03-29 13:59:53 -0400

    Adding avx512popcnt.

--- benchmark/linux/instrumented_benchmark.cpp
+++ benchmark/linux/instrumented_benchmark.cpp
@@ -159,6 +159,106 @@ std::string ourfunctionsnames[NUMBEROFFNC] = {
   "pospopcnt_u16_avx512_mula_unroll4",  "pospopcnt_u16_avx512_mula_unroll8", "purecircuit"
 };
 
+
+static __m256i avx2_popcount(const __m256i vec) {
+
+    const __m256i lookup = _mm256_setr_epi8(
+        /* 0 */ 0, /* 1 */ 1, /* 2 */ 1, /* 3 */ 2,
+        /* 4 */ 1, /* 5 */ 2, /* 6 */ 2, /* 7 */ 3,
+        /* 8 */ 1, /* 9 */ 2, /* a */ 2, /* b */ 3,
+        /* c */ 2, /* d */ 3, /* e */ 3, /* f */ 4,
+
+        /* 0 */ 0, /* 1 */ 1, /* 2 */ 1, /* 3 */ 2,
+        /* 4 */ 1, /* 5 */ 2, /* 6 */ 2, /* 7 */ 3,
+        /* 8 */ 1, /* 9 */ 2, /* a */ 2, /* b */ 3,
+        /* c */ 2, /* d */ 3, /* e */ 3, /* f */ 4
+    );
+
+    const __m256i low_mask = _mm256_set1_epi8(0x0f);
+
+    const __m256i lo  = _mm256_and_si256(vec, low_mask);
+    const __m256i hi  = _mm256_and_si256(_mm256_srli_epi16(vec, 4), low_mask);
+    const __m256i popcnt1 = _mm256_shuffle_epi8(lookup, lo);
+    const __m256i popcnt2 = _mm256_shuffle_epi8(lookup, hi);
+
+    return _mm256_add_epi8(popcnt1, popcnt2);
+}
+
+
+static __m256i popcount(const __m512i v)
+{
+    const __m256i lo = _mm512_extracti64x4_epi64(v, 0);
+    const __m256i hi = _mm512_extracti64x4_epi64(v, 1);
+    const __m256i s  = _mm256_add_epi8(avx2_popcount(lo), avx2_popcount(hi));
+
+    return _mm256_sad_epu8(s, _mm256_setzero_si256());
+}
+
+
+static uint64_t avx2_sum_epu64(const __m256i v) {
+    
+    return _mm256_extract_epi64(v, 0)
+         + _mm256_extract_epi64(v, 1)
+         + _mm256_extract_epi64(v, 2)
+         + _mm256_extract_epi64(v, 3);
+}
+
+
+static void CSA(__m512i* h, __m512i* l, __m512i a, __m512i b, __m512i c) {
+
+  *l = _mm512_ternarylogic_epi32(c, b, a, 0x96);
+  *h = _mm512_ternarylogic_epi32(c, b, a, 0xe8);
+}
+
+
+static uint64_t popcnt_harley_seal(const __m512i* data, const uint64_t size)
+{
+  __m256i total     = _mm256_setzero_si256();
+  __m512i ones      = _mm512_setzero_si512();
+  __m512i twos      = _mm512_setzero_si512();
+  __m512i fours     = _mm512_setzero_si512();
+  __m512i eights    = _mm512_setzero_si512();
+  __m512i sixteens  = _mm512_setzero_si512();
+  __m512i twosA, twosB, foursA, foursB, eightsA, eightsB;
+
+  const uint64_t limit = size - size % 16;
+  uint64_t i = 0;
+
+  for(; i < limit; i += 16)
+  {
+    CSA(&twosA, &ones, ones, _mm512_loadu_si512(data+i+0), _mm512_loadu_si512(data+i+1));
+    CSA(&twosB, &ones, ones, _mm512_loadu_si512(data+i+2), _mm512_loadu_si512(data+i+3));
+    CSA(&foursA, &twos, twos, twosA, twosB);
+    CSA(&twosA, &ones, ones, _mm512_loadu_si512(data+i+4), _mm512_loadu_si512(data+i+5));
+    CSA(&twosB, &ones, ones, _mm512_loadu_si512(data+i+6), _mm512_loadu_si512(data+i+7));
+    CSA(&foursB, &twos, twos, twosA, twosB);
+    CSA(&eightsA,&fours, fours, foursA, foursB);
+    CSA(&twosA, &ones, ones, _mm512_loadu_si512(data+i+8), _mm512_loadu_si512(data+i+9));
+    CSA(&twosB, &ones, ones, _mm512_loadu_si512(data+i+10), _mm512_loadu_si512(data+i+11));
+    CSA(&foursA, &twos, twos, twosA, twosB);
+    CSA(&twosA, &ones, ones, _mm512_loadu_si512(data+i+12), _mm512_loadu_si512(data+i+13));
+    CSA(&twosB, &ones, ones, _mm512_loadu_si512(data+i+14), _mm512_loadu_si512(data+i+15));
+    CSA(&foursB, &twos, twos, twosA, twosB);
+    CSA(&eightsB, &fours, fours, foursA, foursB);
+    CSA(&sixteens, &eights, eights, eightsA, eightsB);
+
+    total = _mm256_add_epi64(total, popcount(sixteens));
+  }
+
+  total = _mm256_slli_epi64(total, 4);     // * 16
+  total = _mm256_add_epi64(total, _mm256_slli_epi64(popcount(eights), 3)); // += 8 * ...
+  total = _mm256_add_epi64(total, _mm256_slli_epi64(popcount(fours),  2)); // += 4 * ...
+  total = _mm256_add_epi64(total, _mm256_slli_epi64(popcount(twos),   1)); // += 2 * ...
+  total = _mm256_add_epi64(total, popcount(ones));
+
+  for(; i < size; i++) {
+    total = _mm256_add_epi64(total, popcount(_mm512_loadu_si512(data+i)));
+  }
+
+
+  return avx2_sum_epu64(total);
+}
+
 void print16(uint32_t *flags) {
     for (int k = 0; k < 16; k++)
         printf(" %8u ", flags[k]);
@@ -200,6 +300,57 @@ computeavgs(std::vector< std::vector<unsigned long long> > allresults) {
         answer[z] /= allresults.size();
     }
     return answer;
+}
+
+void measurepopcnt(uint16_t n, uint32_t iterations, bool verbose) {
+    std::vector<int> evts;
+    std::vector<uint16_t> vdata(n);
+    evts.push_back(PERF_COUNT_HW_CPU_CYCLES);
+    evts.push_back(PERF_COUNT_HW_INSTRUCTIONS);
+    evts.push_back(PERF_COUNT_HW_BRANCH_MISSES);
+    evts.push_back(PERF_COUNT_HW_CACHE_REFERENCES);
+    evts.push_back(PERF_COUNT_HW_CACHE_MISSES);
+    LinuxEvents<PERF_TYPE_HARDWARE> unified(evts);
+    std::vector<unsigned long long> results; // tmp buffer
+    std::vector< std::vector<unsigned long long> > allresults;
+    results.resize(evts.size());
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 0xFFFF);
+
+    n = vdata.size() / (512 / 16) * (512/16);
+    for (uint32_t i = 0; i < iterations; i++) {
+        for (size_t k = 0; k < vdata.size(); k++) {
+            vdata[k] = dis(gen); // random init.
+        }
+        uint64_t expected = popcnt_harley_seal((const __m512i*) vdata.data(), vdata.size() / (512 / 16));       
+        unified.start();
+        uint64_t measured = popcnt_harley_seal((const __m512i*) vdata.data(), vdata.size() / (512 / 16));
+        unified.end(results);
+        assert(measured == expected);
+        allresults.push_back(results);
+    }
+
+    std::vector<unsigned long long> mins = computemins(allresults);
+    std::vector<double> avg = computeavgs(allresults);
+    printf("%-40s\t","avx512popcnt");    
+    if (verbose) {
+        printf("instructions per cycle %4.2f, cycles per 16-bit word:  %4.3f, "
+                "instructions per 16-bit word %4.3f \n",
+                double(mins[1]) / mins[0], double(mins[0]) / n, double(mins[1]) / n);
+        // first we display mins
+        printf("min: %8llu cycles, %8llu instructions, \t%8llu branch mis., %8llu "
+                "cache ref., %8llu cache mis.\n",
+                mins[0], mins[1], mins[2], mins[3], mins[4]);
+        printf("avg: %8.1f cycles, %8.1f instructions, \t%8.1f branch mis., %8.1f "
+                "cache ref., %8.1f cache mis.\n",
+                avg[0], avg[1], avg[2], avg[3], avg[4]);
+    } else {
+        printf("cycles per 16-bit word:  %4.3f \n", double(mins[0]) / n);
+    }
+
+     
 }
 
 
@@ -332,7 +483,7 @@ int main(int argc, char **argv) {
         if (verbose)
             printf("\n");
     }
-
+    measurepopcnt(n, iterations, verbose);
     if (!verbose)
         printf("Try -v to get more details.\n");
 
