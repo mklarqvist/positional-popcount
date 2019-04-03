249eafbf4255afc4da7ccdfbb96666e6d727aef4
Marcus D. R. Klarqvist <mk819@cam.ac.uk>
2019-04-03 19:59:21 +0100

    typo

--- pospopcnt.c
+++ pospopcnt.c
@@ -23,10 +23,9 @@
 int pospopcnt_u16(const uint16_t* data, uint32_t len, uint32_t* flags) {
 #if POSPOPCNT_SIMD_VERSION >= 6
     if (len < 32) return(pospopcnt_u16_sse_sad(data, len, flags)); // small
-    else if (len < 128) return(pospopcnt_u16_sse_mula_unroll8(data, len, flags)); // medium
-    else if (len < 1024) return pospopcnt_u16_avx512_mula2(data, len, flags)); // medium2
-    else if (len < 8192) return pospopcnt_u16_avx512_mula3(data, len, flags)); // medium3
-    return(pospopcnt_u16_avx512_csa(data, len, flags));
+    else if (len < 512) return(pospopcnt_u16_sse_mula_unroll8(data, len, flags)); // medium
+    else if (len < 4096) return(pospopcnt_u16_avx512_mula3(data, len, flags)); // medium3
+    else return(pospopcnt_u16_avx512_csa(data, len, flags));
 #elif POSPOPCNT_SIMD_VERSION >= 5
     if (len < 128) return(pospopcnt_u16_sse_sad(data, len, flags)); // small
     else if (len < 1024) return(pospopcnt_u16_avx2_mula_unroll8(data, len, flags)); // medium
