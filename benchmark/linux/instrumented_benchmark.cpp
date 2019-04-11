f0f6d98f12b5c1524efa4001e3b8dec3213c261c
Daniel Lemire <lemire@gmail.com>
2019-04-11 18:06:26 -0400

    Measuring the counter overhead.

--- benchmark/linux/instrumented_benchmark.cpp
+++ benchmark/linux/instrumented_benchmark.cpp
@@ -254,6 +254,43 @@ void measurepopcnt(uint32_t n, uint32_t iterations, bool verbose) {
     }
 }
 #endif
+void measureoverhead(uint32_t n, uint32_t iterations, bool verbose) {
+    std::vector<int> evts;
+    evts.push_back(PERF_COUNT_HW_CPU_CYCLES);
+    evts.push_back(PERF_COUNT_HW_INSTRUCTIONS);
+    evts.push_back(PERF_COUNT_HW_BRANCH_MISSES);
+    evts.push_back(PERF_COUNT_HW_CACHE_REFERENCES);
+    evts.push_back(PERF_COUNT_HW_CACHE_MISSES);
+    evts.push_back(PERF_COUNT_HW_REF_CPU_CYCLES);
+    LinuxEvents<PERF_TYPE_HARDWARE> unified(evts);
+    std::vector<unsigned long long> results; // tmp buffer
+    std::vector< std::vector<unsigned long long> > allresults;
+    results.resize(evts.size());
+    
+    for (uint32_t i = 0; i < iterations; i++) {
+        unified.start();
+        unified.end(results);
+        allresults.push_back(results);
+    }
+
+    std::vector<unsigned long long> mins = compute_mins(allresults);
+    std::vector<double> avg = compute_averages(allresults);
+    printf("%-40s\t","nothing");    
+    if (verbose) {
+        printf("instructions per cycle %4.2f, cycles per 16-bit word:  %4.3f, "
+               "instructions per 16-bit word %4.3f \n",
+                double(mins[1]) / mins[0], double(mins[0]) / n, double(mins[1]) / n);
+        // first we display mins
+        printf("min: %8llu cycles, %8llu instructions, \t%8llu branch mis., %8llu "
+               "cache ref., %8llu cache mis.\n",
+                mins[0], mins[1], mins[2], mins[3], mins[4]);
+        printf("avg: %8.1f cycles, %8.1f instructions, \t%8.1f branch mis., %8.1f "
+               "cache ref., %8.1f cache mis.\n",
+                avg[0], avg[1], avg[2], avg[3], avg[4]);
+    } else {
+        printf("cycles per 16-bit word:  %4.3f; ref cycles per 16-bit word: %4.3f \n", double(mins[0]) / n, double(mins[5]) / n);
+    }
+}
 
 static void print_usage(char *command) {
     printf(" Try %s -n 100000 -i 15 -v \n", command);
@@ -320,6 +357,7 @@ int main(int argc, char **argv) {
 #if POSPOPCNT_SIMD_VERSION >= 5
     measurepopcnt(n, iterations, verbose);
 #endif
+    measureoverhead(n, iterations, verbose);
    
     for (size_t k = 0; k < PPOPCNT_NUMBER_METHODS; k++) {
         printf("%-40s\t", pospopcnt_u16_method_names[k]);
